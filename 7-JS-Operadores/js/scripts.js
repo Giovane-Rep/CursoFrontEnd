547534ab4d8468cf346768701377f2444c5a9cd6
GiovaneDiasRocha <66972321+GiovaneDiasRocha@users.noreply.github.com>
2023-12-19 11:58:40 -0300

    first commit

---
+++ 7-JS-Operadores/js/scripts.js
@@ -0,0 +1,61 @@
+// Variáveis
+let nome;
+
+nome = "Teste";
+
+let a = 2, b = 4, c = 6;
+let result = 0;
+
+result = b * c / a
+
+console.log(result);
+console.log(nome);
+
+// função prompt() - Lê um dado inserido pelo usuário no console
+
+// let x = prompt("Digite um número: ");
+// console.log(x);
+
+// função alert() - Exibe um popup na tela
+
+//exemplo: alert("Error 404");
+
+// Math
+
+console.log (Math.max(10, 2, 23, 1, 24, 5, 8));
+console.log(Math.floor(12.53));
+console.log(Math.ceil(12.53));
+
+//IF 
+
+// const x = 12
+
+// let num = prompt("Digíte um número");
+
+// if (num < x) {
+//     console.log("O valor digitado é menor que 12");
+// } if (num > x) { 
+//     console.log("O valor digitado é maior que 12");
+// } else if(num == 12) {
+//     console.log("O valor digitado é igual a 12");
+// }
+
+//WHILE
+
+// let num = 0;
+
+// while (num < 10) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num--;
+// } while (num > -1);
+
+//FOR
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
